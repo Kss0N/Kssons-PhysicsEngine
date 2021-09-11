6f352c98bf2630398ff28dfc62c123232e7bafdb
Kss0N <76012325+Kss0N@users.noreply.github.com>
2021-09-11 22:52:27 +0200

    Create elementarytypes.hpp

---
+++ elementarytypes.hpp
@@ -0,0 +1,14 @@
+#pragma once
+namespace P_Types {
+ 
+  using Int8    = signed char;      //8  bit signed integer.
+  using uInt8   = unsigned char;    //8  bit unsigned integer.
+  using Int16   = signed short int; //16 bit signed integer.
+  using uInt16  = unsigned short;   //16 bit unsigned integer.
+  using Int32   = signed int;       //32 bit signed integer.
+  using uInt32  = unsigned int;     //32 bit unsigned integer.
+  using Int64   = signed long int;  //64 bit signed integer.
+  using uInt64  = unsigned long int;//64 bit unsigned integer.
+  
+  using Float = float;
+};
