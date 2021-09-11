4a104c21674407220ed0cadd920ba08866e5e3be
Kss0N <76012325+Kss0N@users.noreply.github.com>
2021-09-12 00:07:06 +0200

    Update complex.hpp

--- complex.hpp
+++ complex.hpp
@@ -1,3 +1,17 @@
 #pragma once
-#include <Elementary_types.hpp>
+#include <elementarytypes.hpp>
 
+namespace P{
+  using P_Types::Float;
+  using imaginary = P::Float;
+  
+  class Complex{
+    P::Float r;
+    imaginary i;
+    
+    Complex(Float real, imaginary i) : r(real), i(i) {};
+    Complex() : r(0), i(0) {};
+    
+  };
+  
+}:
