ca6f441226abbfbd8e9f9db0acfead7638656386
Kss0N <76012325+Kss0N@users.noreply.github.com>
2021-09-12 18:15:38 +0200

    Add files via upload

---
+++ elementaryTypes.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+//Elementary Types
+namespace E_Types {
+	using Int8		= signed char;			//8  bit signed	integer. 
+	using uInt8		= unsigned char;		//8  bit unsigned integer.
+	using Int16		= signed short int;		//16 bit signed integer. 
+	using uInt16	= unsigned short int;	//16 bit unsigned integer.
+	using Int32		= signed int;			//32 bit signed integer. 
+	using uInt32	= unsigned int;			//32 bit unsigned integer.
+	using Int64		= signed long int;		//64 bit signed integer. 
+	using uInt64	= unsigned long int;	//64 bit unsigned integer.
+}
