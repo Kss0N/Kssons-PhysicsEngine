ec4fbf95849ed7c5f8df8810cfd469a53836a17a
Kss0N <76012325+Kss0N@users.noreply.github.com>
2021-09-12 12:17:11 +0200

    Update Quarternion.hpp
    Quarternions are used for rendering.

--- Quarternion.hpp
+++ Quarternion.hpp
@@ -1,2 +1,16 @@
 #pragma once
-#include "Complex.hpp"
+#include "complex.hpp"
+
+namespace P{
+  using jmaginary = P::Float;
+  using kmaginary = P::Float;
+  
+  class Quarternion : private Complex {
+    jmaginary j;
+    kmaginary k;
+    public:
+      Quarternion(P::Float real, P::imaginary i, P::jmaginary j, P::kmaginary k) : r(real), i(i), j(j), k(k) {};
+      Quarternion() : Quarternion(0,0,0,0) {};
+    
+  };
+};
