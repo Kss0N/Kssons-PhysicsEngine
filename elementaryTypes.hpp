a63cd089190ccf12344359200f0a94cc25015465
Kss0N <76012325+Kss0N@users.noreply.github.com>
2021-09-15 08:36:45 +0200

    Update elementaryTypes.hpp

--- elementaryTypes.hpp
+++ elementaryTypes.hpp
@@ -5,11 +5,11 @@ namespace E_Types {
 	using Int8		= signed char;			//8  bit signed	integer. 
 	using uInt8		= unsigned char;		//8  bit unsigned integer.
 	using Int16		= signed short int;		//16 bit signed integer. 
-	using uInt16	= unsigned short int;	//16 bit unsigned integer.
+	using uInt16		= unsigned short int;	//16 bit unsigned integer.
 	using Int32		= signed int;			//32 bit signed integer. 
-	using uInt32	= unsigned int;			//32 bit unsigned integer.
+	using uInt32		= unsigned int;			//32 bit unsigned integer.
 	using Int64		= signed long int;		//64 bit signed integer. 
-	using uInt64	= unsigned long int;	//64 bit unsigned integer.
+	using uInt64		= unsigned long int;	//64 bit unsigned integer.
 	
 	using Float = float;
 }
